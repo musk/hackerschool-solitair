20425ff864bffc219d42a397418a1232a9b20370
slanger <stefan.langer@payback.net>
2023-03-28 09:21:50 +0200

    Added basic playing field

--- solitair.py
+++ solitair.py
@@ -1,22 +1,48 @@
-from cards import Karte, Stapel, AnlageStapel, AblageStapel, Farbe
+from cards import Karte, Stapel, AnlageStapel, AblageStapel, Farbe, KartenTyp
+from ascii import AsciiScreen, AsciiKarte
 
 
 class Solitair(object):
     def __init__(self) -> None:
-        self.stapel = Stapel()
+        self.ziehStapel = Stapel(karten=[Karte(col, type) for col in list(Farbe)
+                                         for type in list(KartenTyp)])
+        self.ablageStapel = Stapel()
         self.ablageHerz = AblageStapel(farbe=Farbe.HERZ)
         self.ablageKaro = AblageStapel(farbe=Farbe.KARO)
         self.ablageKreuz = AblageStapel(farbe=Farbe.KREUZ)
         self.ablagePik = AblageStapel(farbe=Farbe.PIK)
         self.anlageStapel = [AnlageStapel() for i in range(7)]
+        self.screen = AsciiScreen(width=70, height=35)
+
+    def _draw_card(self, karte: Karte) -> str:
+        blatt = AsciiKarte.back()
+        if karte:
+            blatt = AsciiKarte.front(karte)
+        return blatt
+    
+    def _draw_anlage_stapel(self, stapel: AnlageStapel) -> str:
+        return self._draw_card(stapel.top())
 
     def _draw(self):
-        pass
+        for idx, a in enumerate([self.ablageHerz, self.ablageKaro, self.ablagePik, self.ablageKreuz]):
+            self.screen.write_to_screen(self._draw_card(
+                a.top()), AsciiKarte.width()*idx, 0)
+
+        self.screen.write_to_screen(self._draw_card(
+            self.ablageStapel.top()), self.screen.width - AsciiKarte.width()*2, 0)
+        self.screen.write_to_screen(self._draw_card(
+            self.ziehStapel.top()), self.screen.width - AsciiKarte.width(), 0)
+
+        for idx, a in enumerate(self.anlageStapel):
+            self.screen.write_to_screen(self._draw_anlage_stapel(a), AsciiKarte.width()*idx, AsciiKarte.height()+3)
+
+        self.screen.write_to_screen(self._menu(), 0, AsciiKarte.height() + 26)
+        self.screen.print()
 
     def _menu(self):
-        return """
-z: Karte vom Stapel ziehen                  a: Karte auf Stapel x anlegen       l: Karte auf Ablagestapel legen 
-m: Karten von Stapel x zu y verschieben     x: Spiel beenden"""
+        return """─────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+ z: ziehen          a: anlegen          l: ablegen 
+ m: verschieben     x: Spiel beenden"""
 
     def _waehle_anlage(self) -> AnlageStapel:
         nummer = -1
@@ -45,12 +71,12 @@ m: Karten von Stapel x zu y verschieben     x: Spiel beenden"""
         eingabe: str = input("Zug? ").lower().strip()
         if eingabe == "a":
             a = self._waehle_anlage()
-            k = self.stapel.ziehen()
+            k = self.ziehStapel.ziehen()
             a.anlegen(k)
         elif eingabe == "l":
             a = self._waehle_ablage()
             k = self._waehle_anlage()
-            
+
             pass
         elif eingabe == "m":
             pass
@@ -63,17 +89,15 @@ m: Karten von Stapel x zu y verschieben     x: Spiel beenden"""
     def play(self):
         # the game loop
         while True:
+            pass
 
 
 def main():
     '''
     The enty program for the solitair program
     '''
-    deck = Stapel()
-    print(deck)
-    print()
-    deck.shuffle()
-    print(deck)
+    s = Solitair()
+    s._draw()
 
 
 if __name__ == "__main__":
