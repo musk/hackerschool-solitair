201be4542b511a60a0c4fae5ba1f2f3f987cd14c
slanger <stefan.langer@payback.net>
2023-03-30 20:10:32 +0200

    added verschieben, anlegen and increased screen height

--- solitair.py
+++ solitair.py
@@ -22,8 +22,10 @@ class Solitair(object):
         self.ablagePik = AblageStapel(farbe=Farbe.PIK)
         self.anlageStapel = [AsciiStapel(AnlageStapel(
             karten=self._initAnlage(i+1))) for i in range(7)]
-        self.screen = AsciiScreen(width=70, height=35)
+        self.screen = AsciiScreen(width=70, height=AsciiKarte.height()*2 + 33)
         self.navigation = False
+        self.navigation_anlage = False
+        self.navigation_ablage = False
         self.status_msg = ""
 
     def _initAnlage(self, kartenZiehen: int) -> list[Karte]:
@@ -44,21 +46,23 @@ class Solitair(object):
 
         self.screen.write_to_screen(AsciiKarte.print(self.ablageStapel.top()),
                                     self.screen.width - AsciiKarte.width()*2, 0)
-        if self.navigation:
+        if self.navigation_ablage:
             self.screen.write_to_screen(
                 f"[{len(self.anlageStapel)}]", self.screen.width - AsciiKarte.width()*2+1, AsciiKarte.height()+1)
         self.screen.write_to_screen(AsciiKarte.print(self.ziehStapel.top()),
                                     self.screen.width - AsciiKarte.width(), 0)
-        
+
         for idx, a in enumerate(self.anlageStapel):
             self.screen.write_to_screen(
                 a.printFanned(), AsciiKarte.width()*idx, AsciiKarte.height()+4)
-            if self.navigation:
+            if self.navigation_anlage:
                 self.screen.write_to_screen(
                     f"[{idx}]", AsciiKarte.width()*idx+1, AsciiKarte.height()+3)
 
-        self.screen.write_to_screen(self._menu(), 0, AsciiKarte.height() + 26)
-        self.screen.write_to_screen(self.status_msg, 0, AsciiKarte.height() + 24)
+        self.screen.write_to_screen(
+            self._menu(), 0, AsciiKarte.height()*2 + 29)
+        self.screen.write_to_screen(
+            self.status_msg, 0, AsciiKarte.height()*2 + 27)
         self.screen.print()
 
     def _menu(self):
@@ -82,10 +86,6 @@ class Solitair(object):
     def _write_message(self, text: str):
         self.status_msg = text
 
-    def _end(self):
-        print("Danke das sie Solitair gespielt haben!")
-        exit(0)
-
     def _get_number(self, msg: str, range: range) -> int:
         num = ""
         while True:
@@ -94,10 +94,21 @@ class Solitair(object):
                 return int(num)
             else:
                 self._write_message("Bitte geben sie eine gültige Zahl an!")
+                self._draw()
 
-    def _toggle_navigation(self):
-        self.navigation = not self.navigation
+    def _show_navigation(self, withAblage=False):
+        self.navigation_anlage = True
+        if withAblage:
+            self.navigation_ablage = True
         self._draw()
+
+    def _hide_navigation(self):
+        self.navigation_anlage = False
+        self.navigation_ablage = False
+
+    def _end(self):
+        print("Danke das sie Solitair gespielt haben!")
+        exit(0)
 
     def _umdrehen(self):
         if self.ziehStapel.leer():
@@ -107,7 +118,8 @@ class Solitair(object):
                 k = self.ablageStapel.ziehen()
             self.ziehStapel.shuffle()
         else:
-            self._write_message("Umdrehen nicht möglich, es sind noch Karten auf dem Stapel!")
+            self._write_message(
+                "Umdrehen nicht möglich, es sind noch Karten auf dem Stapel!")
 
     def _ziehen(self):
         k = self.ziehStapel.ziehen()
@@ -115,11 +127,10 @@ class Solitair(object):
             self.ablageStapel.anlegen(k.aufdecken())
 
     def _ablegen(self):
-        self._toggle_navigation()
+        self._show_navigation(withAblage=True)
         idx = self._get_number(
-            "Welchen Stapel möchten sie ablegen?", range(0, len(self.anlageStapel)+1))
-        self._toggle_navigation()
-        k: Karte = None
+            "Welchen Stapel möchten sie ablegen? ", range(0, len(self.anlageStapel)+1))
+        self._hide_navigation()
         if idx < len(self.anlageStapel):
             stapel = self.anlageStapel[idx].stapel
         elif idx == len(self.anlageStapel):
@@ -130,24 +141,54 @@ class Solitair(object):
         if k:
             for s in [self.ablageHerz, self.ablageKaro, self.ablagePik, self.ablageKreuz]:
                 if k.farbe == s.farbe:
-                    if s.anlegbar(k):  
+                    if s.anlegbar(k):
                         s.anlegen(stapel.ziehen())
                         stapel.aufdecken()
                         break
                     else:
                         msg = f"Karte {k.farbe.blatt} {k.typ.name.capitalize()} kann nicht abgelegt werden!"
             self._write_message(msg)
-                
+
     def _anlegen(self):
-        pass
+        k = self.ablageStapel.top()
+        if k:
+            self._show_navigation(withAblage=False)
+            idx = self._get_number(f"Wo möchten sie die Karte {k.farbe.blatt} {k.typ.blatt} anlegen? ",
+                                   range(0, len(self.anlageStapel)))
+            self._hide_navigation()
+            stapel = self.anlageStapel[idx].stapel
+
+            if stapel.anlegbar(k):
+                stapel.anlegen(self.ablageStapel.ziehen())
+            else:
+                self._write_message(
+                    f"Karte {k.farbe.blatt} {k.typ.blatt} kann nicht an Stapel [{idx}] angelegt werden!")
 
     def _verschieben(self):
-        pass
+        self._show_navigation(withAblage=False)
+        von = self._get_number(f"Von welchem Stapel wollen sie Karten verschieben? ",
+                               range(0, len(self.anlageStapel)))
+        vonStapel = self.anlageStapel[von].stapel
+        self._write_message(f"Verschieben von Stapel {von}")
+        self._draw()
+        zu = self._get_number(f"Zu welchem Stapel wollen sie die Karten verschieben? ",
+                              range(0, len(self.anlageStapel)))
+        zuStapel = self.anlageStapel[zu].stapel
+        self._hide_navigation()
+        if not vonStapel.verschieben_nach(zuStapel):
+            self._write_message(
+                f"Verschieben von Stapel {von} auf Stapel {zu} nicht möglich!")
+        else:
+            self._write_message("")
+
+    def _won(self):
+        
+        return False
 
     def play(self):
         self._draw()
         # the game loop
-        while True:
+        while not self._won():
             self.status_msg = ""
             self._input()
             self.screen.clear_screen()
